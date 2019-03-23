76182415cade09d25817ccd4f90b1eb991621f12
John Reese <john@reese.dev>
2019-03-23 12:03:52 -0400

    Simplify configuration

--- catfact-client/src/app/app.component.ts
+++ catfact-client/src/app/app.component.ts
@@ -6,14 +6,25 @@ import { CatFactRequest, CatFactResponse } from '../proto/catfact_pb';
 @Component({
   selector: 'app-root',
   template: `
-    <div id="factPlaceholder">Fact Goes Here</div>
-    <button type="button" class="btn btn-primary">Get Fact</button>
+    <div id="factPlaceholder">{{ currentFact }}</div>
+    <button type="button" class="btn btn-primary" (click)="getFact()">Get Fact</button>
   `
 })
 export class AppComponent {
 
-  public ngOnInit(): void {
+  public currentFact: string = "test";
+  private catFactClient: CatFactServiceClient;
 
+  public ngOnInit(): void {
+    this.catFactClient = new CatFactServiceClient("http://localhost:8080", null, null)
   }
 
+  private getFact(): void {
+    const catFactRequest: CatFactRequest = new CatFactRequest();
+    const metaData: any = {}
+
+    this.catFactClient.get(catFactRequest, metaData, (err, response: CatFactResponse) => {
+      this.currentFact = response.fact;
+    });
+  }
 }
--- server.go
+++ server.go
@@ -9,17 +9,23 @@ import (
 )
 
 func main() {
-	const port = 1337
+	const port = 7777
+
+	log.Printf("Attempting to listen on port: %d", port)
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatalf("Failed to listen on specified port: %v", err)
+		log.Fatalf("Failed to listen on port: %d (%v)", port, err)
 	}
+	defer lis.Close()
 
 	catFactServer := catfact.Server{}
 	grpcServer := grpc.NewServer()
+	log.Printf("Service created.")
 
 	catfact.RegisterCatFactServiceServer(grpcServer, &catFactServer)
+	log.Printf("Service registered.")
 
+	log.Printf("Attempting to start service.")
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to start service: %s", err)
 	} else {
