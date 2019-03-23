435a41eeecf55e483126bb5e06ae81d3f4c5846f
John Reese <john@reese.dev>
2019-03-22 23:41:00 -0400

    Initial service code

--- catfact/cat_fact.go
+++ catfact/cat_fact.go
@@ -1,39 +1,16 @@
 package catfact
 
 import (
-	"github.com/satori/go.uuid"
 	"golang.org/x/net/context"
 	"log"
 )
 
-// gRPC server
+// Server contains all available procedures
 type Server struct {
-	Todos []*TodoObject
 }
 
-func (s *Server) AddTodo(ctx context.Context, newTodo *AddTodoParams) (*TodoObject, error) {
-	log.Printf("Received new task %s", newTodo.Task)
-	todoObject := &TodoObject{
-		Id:   uuid.NewV1().String(),
-		Task: newTodo.Task,
-	}
-	s.Todos = append(s.Todos, todoObject)
-	return todoObject, nil
-}
-
-func (s *Server) GetTodos(ctx context.Context, _ *GetTodoParams) (*TodoResponse, error) {
-	log.Printf("get tasks")
-	return &TodoResponse{Todos: s.Todos}, nil
-}
-
-func (s *Server) DeleteTodo(ctx context.Context, delTodo *DeleteTodoParams) (*DeleteResponse, error) {
-	var updatedTodos []*TodoObject
-	for index, todo := range s.Todos {
-		if todo.Id == delTodo.Id {
-			updatedTodos = append(s.Todos[:index], s.Todos[index+1:]...)
-			break
-		}
-	}
-	s.Todos = updatedTodos
-	return &DeleteResponse{Message: "success"}, nil
+// Get receiver to return a random cat fact
+func (s *Server) Get(ctx context.Context) (*CatFactResponse, error) {
+	log.Printf("get fact")
+	return &CatFactResponse{Fact: "cats are pretty"}, nil
 }
