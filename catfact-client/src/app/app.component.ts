589b0b7a9d2cd07b017e11eb5b4ae5c8c020c6fe
John Reese <john@reese.dev>
2019-03-24 15:17:06 -0400

    Remove console log

--- catfact-client/src/app/app.component.ts
+++ catfact-client/src/app/app.component.ts
@@ -24,7 +24,6 @@ export class AppComponent {
     const metaData: any = {}
 
     this.catFactClient.get(catFactRequest, metaData, (err, response: CatFactResponse) => {
-      console.log(response);
       this.currentFact = response.getFact();
     });
   }
